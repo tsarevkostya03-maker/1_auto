7491385dd4112f56e73617f515441394890b222e
tsarevkostya03-maker <tsarevkostya03@gmail.com>
2026-06-30 19:01:42 +0300

    Switch to TestNG in testng branch with failing test for 1000 rubles

--- src/test/java/org/example/CashbackHackServiceTest.java
+++ src/test/java/org/example/CashbackHackServiceTest.java
@@ -1,7 +1,7 @@
 package org.example;
 
-import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
 
